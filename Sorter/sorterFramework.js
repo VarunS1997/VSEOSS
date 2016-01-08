75616d7568c67dde9e4380b1908d71de36deb62d
VarunS1997 <VarunS1997@users.noreply.github.com>
2016-01-08 11:00:50 -0800

    Update sorterFramework.js

--- Sorter/sorterFramework.js
+++ Sorter/sorterFramework.js
@@ -1,3 +1,43 @@
-var n1 = 0;
-var n2 = 0;
-var auxn = -1;
+var speed = 1000; //speed between steps, ms
+
+var n1 = 0; //primary node pointer
+var n2 = 0; //secondary node pointer
+var auxn = -1; //auxillary node pointer
+
+var size = 10;
+var data = new Array(size);
+for(var i = 0; i < size; i++){
+  data[i] = i + 1;
+}
+
+for(var i = 0; i < size * 2; i++){
+   var x = Math.trunc(Math.random() * size);
+   var y = Math.trunc(Math.random() * size);
+  
+  swap(x, y);
+}
+
+var algorithm = insertSort(); //set to sorting algorithm
+var sort = function() { algorithm(); }; //reference pointer to algorithm
+
+function swap(x, y){ //useful for various algorithms
+  var z = data[x];
+  data[x] = data[y];
+  data[y] = z;
+}
+
+function reset(newSize){
+  size = newSize;
+  data = new Array(size);
+  
+  for(var i = 0; i < size; i++){
+    data[i] = i + 1;
+  }
+  
+  for(var i = 0; i < size * 2; i++){
+     var x = Math.trunc(Math.random() * size);
+     var y = Math.trunc(Math.random() * size);
+    
+    swap(x, y);
+  }
+}
