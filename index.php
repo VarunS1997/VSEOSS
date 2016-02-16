7ae9021e89250bb60d0c1731560967d6320c4350
VarunS1997 <varun.1997.singh@gmail.com>
2016-02-16 04:35:11 -0800

    Basic Homepage UI Improvements

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    
 ?>
 
 <!DOCTYPE html>
@@ -7,10 +7,14 @@
     <head>
         <meta charset="utf-8" />
         <title>VSEOSS Gateway</title>
+        <link rel="stylesheet" type="text/css" href="generalCSS.css" />
     </head>
     <body>
-        <a href="phySim">PhySim</a>
-        <a href="sorter">Sorter</a>
+        <div id="basicNav">
+            <a href="phySim">PhySim</a><br />
+            <a href="sorter">Sorter</a><br />
+            <a href="https://github.com/VarunS1997/VSEOSS">Github Repository</a>
+        </div>
     </body>
 </html>
  
