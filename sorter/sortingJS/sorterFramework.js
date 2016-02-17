c2d7bd18b7ea71e95ff6d7512a578e944d421156
Varun Singh <varun.1997.singh@gmail.com>
2016-02-17 09:49:31 -0800

    Bug Fix with dataSize Changes

--- sorter/sortingJS/sorterFramework.js
+++ sorter/sortingJS/sorterFramework.js
@@ -30,7 +30,7 @@ function init() {
     auxn = -1;
 
     document.getElementById("speedInput").setAttribute("value", speed);
-    //document.getElementById("sizeInput").setAttribute("value", dataSize);
+    document.getElementById("sizeInput").setAttribute("value", dataSize);
 
     //this order because each of these inits needs the previous done
     initDisplay();
@@ -38,7 +38,7 @@ function init() {
     initTimer();
 
     //color
-    modifyPrimaries(0, 0);
+    modifyPrimaries(-n1, -n2);
 
     //
     //initialization supplementary functions
@@ -74,7 +74,7 @@ function init() {
         for (var i = 0; i < data.length * 2; i++) {
             var x = Math.floor(Math.random() * dataSize);
             var y = Math.floor(Math.random() * dataSize);
-            swap(x, y, false);
+            swap(x, y);
         }
     }
 
