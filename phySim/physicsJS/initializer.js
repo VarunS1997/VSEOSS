acdf8a6c15a6770a42181b212a1f9e94dd4c51b6
VarunS1997 <varun.1997.singh@gmail.com>
2016-06-07 18:14:43 -0700

    Feeds and minor organizing

--- phySim/physicsJS/initializer.js
+++ phySim/physicsJS/initializer.js
@@ -2,16 +2,22 @@
 
 // physics globals
 var particles;
+var totalKE = 0;
+
 var ticker = setInterval(physics_timePass, 7); //7 ms because it increases animation fluidity
 var ready = false; //ready to pass time?
+
 var worldX; //size of world in X direction
 var worldY; //size of world in y direction
+
 var collisions = false;
 
 //UI globals
 var clock_angle_rads = Math.PI / 2; //angle of clock hand in radiants
 var clock_radius; //radius of clock
 var clock_hand; //clock hand
+
+var UIFeeds = [];
 
 var pausePlayer;
 
@@ -20,25 +26,34 @@ console.log("Loaded Physics Engine");
 function init(){
     console.log("Initializing Physics Engine...");
 
-    setTimeout(function(){ physics_toggleCollisions(); }, 1000 * 1/temporalScale);
-    var svgE = document.getElementById("particleCanvas").childNodes;
+    // create particles and apply initial conditions
+    physics_init();
 
+    UI_init();
+
+    console.log("Physics Engine Ready");
+    console.log("Collisions: " + (collisions ? "ON" : "OFF") + " --- Time: " + (time ? "ON" : "OFF"));
+};
+
+// physics functions
+function physics_init(){
+    var svgE = document.getElementById("particleCanvas").childNodes;
     particles = [svgE.length];
-    clock_hand = document.getElementById("clock");
-    pausePlayer = document.getElementById("pausePlay");
+
     worldX = document.getElementById("particleCanvas").clientWidth;
     worldY = document.getElementById("particleCanvas").clientHeight;
 
-    pausePlayer.addEventListener("click", clock_pausePlay);
-
-    //create particles and apply initial conditions
     for (var i = 0; 1 + 2 * i < svgE.length; i++) {
         if(svgE[1 + 2 * i].tagName != null && svgE[1 + 2 * i].tagName.toLowerCase() === "circle"){
             //1+2*i is formula for the "(i+1)th" circle
             //we also tell the particles they start at the origin
-            var particle1 = new Particle(1, 0, 0, parseInt(svgE[1 + 2 * i].getAttribute("r")), svgE[1 + 2 * i]);
+            var radius = parseInt(svgE[1 + 2 * i].getAttribute("r"));
+            var xPos = (parseInt(svgE[1 + 2 * i].getAttribute("cx")) - 50) * spacialScale;
+            var yPos = (parseInt(svgE[1 + 2 * i].getAttribute("cy")) - 50) * spacialScale;
 
-            particle1.setForce(0, -1 * particle1.mass);
+            var particle1 = new Particle(1, xPos, yPos, radius, svgE[1 + 2 * i]);
+
+            particle1.setForce(0, -0 * particle1.mass);
             particle1.setCOR(1);
             particle1.setVelocity((Math.random() - .5) * 100, (Math.random() - .5) * 10);
 
@@ -48,19 +63,9 @@ function init(){
         }
     }
 
-    clock_hand.onload = function () {
-        clock_hand = clock.contentDocument.getElementById("tick");
-        if (clock_radius == null) { //placed here to ensure clock is loaded correctly
-            ready = true;
-            clock_radius = 250;
-        }
-    };
-
-    console.log("Physics Engine Ready");
-    console.log("Collisions: " + (collisions ? "ON" : "OFF") + " --- Time: " + (time ? "ON" : "OFF"));
+    setTimeout(function(){ physics_toggleCollisions(); }, 1000 * 1/temporalScale);
 };
 
-// physics functions
 function physics_timePass() {
     if (time && ready) {
         ready = false;
@@ -75,15 +80,23 @@ function physics_timePass() {
             }
         }
 
+        totalKE = 0;
         //cause passage of time
         for(var i = 0; i < particles.length; i++){
             particles[i].experienceTime();
+            totalKE += .5 * particles[i].mass + particles[i].velX * particles[i].velX + particles[i].velY * particles[i].velY;
         }
+        totalKE = Math.round(totalKE * 1000) / 1000;
 
-        clock_update();
+        UI_update();
 
         ready = true;
     }
+};
+
+function physics_updateSize(){
+    worldX = document.getElementById("particleCanvas").clientWidth;
+    worldY = document.getElementById("particleCanvas").clientHeight;
 };
 
 function physics_toggleTime(){
@@ -94,14 +107,32 @@ function physics_toggleTime(){
 function physics_toggleCollisions(){
     collisions = !collisions;
     console.log("Collisions: " + (collisions ? "ON" : "OFF"));
-}
-
-function physics_updateSize(){
-    worldX = document.getElementById("particleCanvas").clientWidth;
-    worldY = document.getElementById("particleCanvas").clientHeight;
 };
 // UI Functions
-function clock_update(){
+function UI_init(){
+    clock_hand = document.getElementById("clock");
+
+    pausePlayer = document.getElementById("pausePlay");
+
+    UIFeeds = document.getElementsByClassName("ui-feed");
+
+    pausePlayer.addEventListener("click", UI_pausePlay);
+
+    clock_hand.onload = function () {
+        clock_hand = clock.contentDocument.getElementById("tick");
+        if (clock_radius == null) { //placed here to ensure clock is loaded correctly
+            ready = true;
+            clock_radius = 250;
+        }
+    };
+};
+
+function UI_update(){
+    UI_updateClock();
+    UI_updateFeeds();
+};
+
+function UI_updateClock(){
     //clock math
     clock_hand.setAttribute("x2", 255.5 + clock_radius * Math.cos(clock_angle_rads));
     clock_hand.setAttribute("y2", 255 - clock_radius * Math.sin(clock_angle_rads));
@@ -112,7 +143,32 @@ function clock_update(){
     clock_angle_rads -= (Math.PI / 360) * temporalScale;
 };
 
-function clock_pausePlay() {
+function UI_updateFeeds(){
+    //remove all elements, then add new info
+    var currentFeed;
+    var typeOfData;
+    var newData;
+
+    for(var i = 0; i < UIFeeds.length; i++){
+        currentFeed = UIFeeds[i];
+
+        while(currentFeed.firstChild){
+            currentFeed.removeChild(currentFeed.firstChild);
+        }
+
+        typeOfData = currentFeed.id;
+
+        if(typeOfData.includes("KE")){
+            newData = document.createTextNode(totalKE + " J");
+        } else{
+            newData = document.createTextNode("Error: Unknown Feed");
+        }
+
+        currentFeed.appendChild(newData);
+    }
+};
+
+function UI_pausePlay() {
     if (time) {
         pausePlayer.setAttribute("src", "play.svg");
     } else {
